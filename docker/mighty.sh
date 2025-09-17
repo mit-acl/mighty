82dbc1a51b7d64d9130e120a74af5ad2d4f7552c
kotakondo <kkondo@mit.edu>
2025-09-17 13:04:20 -0400

    minor

--- docker/mighty.sh
+++ docker/mighty.sh
@@ -5,7 +5,7 @@ source /home/kkondo/code/mighty_ws/install/setup.bash
 source /usr/share/gazebo/setup.sh
 
 # (0) sim in static environment
-python3 src/mighty/launch/run_single_sim.py --env easy_forest
+python3 /home/kkondo/code/mighty_ws/src/mighty/launch/run_single_sim.py --env easy_forest
 
 # (1) sim in dynamic environment
-# python3 src/mighty/launch/run_single_sim.py --env dynamic_forest
+# python3 run_single_sim.py --env dynamic_forest
