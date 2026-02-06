dc4234ba63b073f1b1f172f49be94b536f44aadd
kotakondo <kkondo@mit.edu>
2026-02-06 15:24:49 -0500

    use the right repo for docker + added no gpu option for docker run

--- setup.sh
+++ setup.sh
@@ -180,7 +180,7 @@ if [ ! -d "$MIGHTY_WS/src/mighty" ]; then
     cd "$MIGHTY_WS/src"
     git clone https://github.com/mit-acl/mighty.git
     cd mighty
-    git switch 4f5bb367dbab0683f1666ebe3838a7c4807c88b7
+    git switch 6237c6524df309e50e34b5501cbef70b2e1c6d80
 else
     echo "MIGHTY already exists, updating..."
     cd "$MIGHTY_WS/src/mighty"
