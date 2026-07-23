e98c9a2db7ec6b803abbcb2285b5fd0ad6664484
Blake Edwards <blakeedwards823@gmail.com>
2026-07-22 21:51:41 -0400

    added explicit run_hw_red_rover.py param depth_pointcloud_topic set to patchwork/nonground

--- scripts/run_hw_red_rover.py
+++ scripts/run_hw_red_rover.py
@@ -118,6 +118,7 @@ def generate_yaml(odom_type: str, rover_name: str, goal_type: int,
             f'ros2 launch global_mapper_ros global_mapper_patchworkpp.launch.py'
             f' hardware:=true ground_robot:=true quad:={rover_name}'
             # f' depth_pointcloud_topic:=livox/lidar'
+            f' depth_pointcloud_topic:=patchworkpp/nonground'
             f' use_sim_time:=false'
             f' pose_topic:=dlio/odom_node/pose'
             f' use_obstacle_tracker:=true'
